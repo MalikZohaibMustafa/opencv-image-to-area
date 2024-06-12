b0a51d16758efd520ad83d9199af9cf476533f82
MALIK ZOHAIB MUSTAFA <56153394+MalikZohaibMustafa@users.noreply.github.com>
2024-06-13 03:50:12 +0500

    ABC

--- app.py
+++ app.py
@@ -187,7 +187,10 @@ def process_image(image, width):
 
         dimA = dA / pixelsPerMetric
         dimB = dB / pixelsPerMetric
-        area = cv2.contourArea(c) / (pixelsPerMetric ** 2)
+
+        dimA = dA / pixelsPerMetric
+        dimB = dB / pixelsPerMetric
+        area = dimA * dimB
 
         cv2.putText(orig, "{:.1f}in".format(dimA), (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
         cv2.putText(orig, "{:.1f}in".format(dimB), (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
